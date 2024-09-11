b5de7855b3f4d89d94a002a4c2c6337db5153678
cheorhyeon <r4560798@naver.com>
2024-09-11 16:24:54 +0900

    샤드CN 적용

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,8 +1,14 @@
+"use client";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons/faArrowRight";
 import { faArrowsToEye } from "@fortawesome/free-solid-svg-icons/faArrowsToEye";
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import { useState } from "react";
 
 export default function PostPage({ params }: { params: { id: string } }) {
+  const [date, setDate] = useState<Date | undefined>();
   const { id } = params;
 
   const post = {
@@ -11,11 +17,22 @@ export default function PostPage({ params }: { params: { id: string } }) {
   };
 
   return (
-    <div className="text-red-500 text-6xl">
+    <div className="text-500 text-6xl">
       <h1>{post.title}</h1>
       <p>{post.content}</p>
       <FontAwesomeIcon icon={faArrowRight}></FontAwesomeIcon>
       <FontAwesomeIcon icon={faArrowsToEye}></FontAwesomeIcon>
+      <Button variant="outline">Button</Button>
+
+      <Calendar
+        mode="single"
+        selected={date}
+        onSelect={setDate}
+        className="rounded-md border"
+      />
+
+      <hr />
+      {date?.toString()}
     </div>
   );
 }
